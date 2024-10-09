e41d83a33346b0bfa1aff944bb0f6442ab8e6940
Anton <datskiiak@yandex.ru>
2024-10-09 13:28:38 +0400

    delete unusing ReferencePoint

--- CoordInputWindow.py
+++ CoordInputWindow.py
@@ -1,5 +1,4 @@
 from PyQt5.QtWidgets import QDialog, QVBoxLayout, QLineEdit, QPushButton, QLabel
-from ReferencePoint import ReferencePoint
 
 class CoordInputWindow(QDialog):
     def __init__(self, point):
